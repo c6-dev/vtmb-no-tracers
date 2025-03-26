a71b9bef0b041308c90e432530093b94b902ca59
c6 <31777460+c6-dev@users.noreply.github.com>
2025-03-26 22:48:02 +0400

    Add project files.

---
+++ no_tracers/dllmain.cpp
@@ -0,0 +1,30 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include "windows.h"
+
+#define UInt32 unsigned long
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    return TRUE;
+}
+
+void SafeWrite32(UInt32 addr, UInt32 data)
+{
+    UInt32	oldProtect;
+
+    VirtualProtect((void*)addr, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
+    *((UInt32*)addr) = data;
+    VirtualProtect((void*)addr, 4, oldProtect, &oldProtect);
+}
+
+extern "C" __declspec(dllexport) void loaded_client() {
+		HMODULE client = GetModuleHandleA("client.dll");
+        if (client != NULL) {
+            UInt32 addr = (UInt32)client + 0x4099A;
+            SafeWrite32(addr, 0xB0E9);
+        }
+    }
+
+

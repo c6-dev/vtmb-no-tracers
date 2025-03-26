6287e264f34c48bd8737106637c7adf0e0848509
c6 <31777460+c6-dev@users.noreply.github.com>
2025-03-26 22:52:04 +0400

    reformat

--- no_tracers/dllmain.cpp
+++ no_tracers/dllmain.cpp
@@ -5,26 +5,26 @@
 #define UInt32 unsigned long
 
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-    return TRUE;
+	return TRUE;
 }
 
 void SafeWrite32(UInt32 addr, UInt32 data)
 {
-    UInt32	oldProtect;
+	UInt32 oldProtect;
 
-    VirtualProtect((void*)addr, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
-    *((UInt32*)addr) = data;
-    VirtualProtect((void*)addr, 4, oldProtect, &oldProtect);
+	VirtualProtect((void*)addr, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
+	*((UInt32*)addr) = data;
+	VirtualProtect((void*)addr, 4, oldProtect, &oldProtect);
 }
 
-extern "C" __declspec(dllexport) void loaded_client() {
-		HMODULE client = GetModuleHandleA("client.dll");
-        if (client != NULL) {
-            UInt32 addr = (UInt32)client + 0x4099A;
-            SafeWrite32(addr, 0xB0E9);
-        }
-    }
-
-
+extern "C" __declspec(dllexport) void loaded_client()
+{
+	HMODULE client = GetModuleHandleA("client.dll");
+	if (client != NULL)
+	{
+		UInt32 addr = (UInt32)client + 0x4099A;
+		SafeWrite32(addr, 0xB0E9);
+	}
+}
